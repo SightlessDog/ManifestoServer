390afea68b7803020677621308b15ce417bebb5b
elyess.eleuch <ely.369@outlook.com>
2021-06-05 14:38:23 +0200

    promise times out

--- src/AbletonController/AbletonController.js
+++ src/AbletonController/AbletonController.js
@@ -3,9 +3,6 @@ const { Ableton } = require("ableton-js");
 const ableton = new Ableton();
 let tracks = [];
 
-ableton.on("connect", (e) => console.log("Connect", e));
-ableton.on("message", (m) => console.log("Message:", m));
-
 const loadTracks = async () => {
   tracks = await ableton.song.get("tracks");
 };
@@ -13,7 +10,10 @@ const loadTracks = async () => {
 const play = async (id) => {
   let trackToPlay = tracks[id];
   try {
-    trackToPlay.set("mute", false);
+    const isTrackMuted = await trackToPlay.get("mute");
+    if (isTrackMuted) {
+      const playing = await trackToPlay.set("mute", false);
+    }
   } catch (err) {
     console.error(err);
   }
@@ -22,7 +22,11 @@ const play = async (id) => {
 const mute = async (id) => {
   let trackToMute = tracks[id];
   try {
-    trackToMute.set("mute", true);
+    const isTrackMuted = await trackToMute.get("mute");
+    if (!isTrackMuted) {
+      const mute = await trackToMute.set("mute", true);
+      return true;
+    }
   } catch (err) {
     console.error(err);
   }
--- src/KafkaConsumer/kafkaConsumer.js
+++ src/KafkaConsumer/kafkaConsumer.js
@@ -8,6 +8,7 @@ const kafka = new Kafka({ brokers });
 const consumer = kafka.consumer({ groupId: "firstPart" });
 const topic = "raspi1";
 let users = [];
+let found = false;
 
 const distanceBetweenCoords = (centerX, centerY, x, y) => {
   return (x - centerX) * (x - centerX) + (y - centerY) * (y - centerY);
@@ -17,27 +18,27 @@ const consume = async (aoi) => {
   await consumer.connect();
   await consumer.subscribe({ topic });
   await consumer.run({
-    eachMessage: ({ message }) => {
+    eachMessage: async ({ message }) => {
+      found = false;
       console.log(`received message: ${message.value}`);
       let string = message.value.toString("utf-8");
       let obj = JSON.parse(string);
-      console.log("the object", obj);
       if (users.length === 0) {
         users.push(obj);
-        console.log("users", users);
       } else {
         users.forEach((user) => {
-          if (obj.id == user.id) {
+          if (obj.id === user.id) {
             user.centerX = obj.centerX;
             user.centerY = obj.centerY;
-          } else {
-            users.push(obj);
+            found = true;
           }
         });
-        console.log("users", users);
+        if (!found) {
+          users.push(obj);
+        }
       }
       if (users.length !== 0) {
-        users.forEach((user) => {
+        users.forEach(async (user) => {
           distance = distanceBetweenCoords(
             aoi.center.x,
             aoi.center.y,
@@ -45,15 +46,7 @@ const consume = async (aoi) => {
             user.centerY
           );
           if (distance < aoi.radius * aoi.radius) {
-            console.log("userId", user);
-            console.log("users", users);
-
-            mute(user.id);
-          } else {
-            console.log("userId", user);
-            console.log("users", users);
-
-            play(user.id);
+            const muted = await play(user.id);
           }
         });
       }
